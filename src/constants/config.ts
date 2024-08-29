ad68b86d01a42d4922031f2ea39551cfcd5ac599
DevViTien <truongnbn.dev@gmail.com>
2024-08-30 05:43:20 +0700

    add favicon and config app layout

---
+++ src/constants/config.ts
@@ -0,0 +1,6 @@
+export const APP_NAME = "DevViTien"
+export const APP_TITLE = "DevViTien | Support building websites"
+export const APP_SLOGAN = "Dev vi tiên, tiền vi hậu"
+
+export const AUTHOR_NAME = "TruongNBN"
+export const AUTHOR_FACEBOOK = "https://www.facebook.com/nguyenbui.namtruong"
